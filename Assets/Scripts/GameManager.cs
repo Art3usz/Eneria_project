a81fd03a817276158e7d441747b224b77a54e77a
Damian Pieczykolan / Art3usz <Damian.Pieczykolan@10g.pl>
2022-04-05 18:09:59 +0200

    cleaninig code

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,29 +1,23 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
 
     private static GameManager _instance;
-    public static GameManager instance
-    {
-        get
-        {
-            if (_instance == null)
-            {
-                _instance = FindObjectOfType<GameManager>();
+    public static GameManager instance {
+        get {
+            if (_instance == null) {
+                _instance = FindObjectOfType<GameManager> ();
             }
 
-            if (_instance == null)
-            {
-                GameObject go = new GameObject("GameManager");
-                go.AddComponent<GameManager>();
+            if (_instance == null) {
+                GameObject go = new GameObject ("GameManager");
+                go.AddComponent<GameManager> ();
             }
             return _instance;
         }
-
     }
-    public void Awake()
-    {
+
+    public void Awake () {
         _instance = this;
     }
 }
